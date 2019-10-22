70a17cdf268e0994621ee7791bc407dfddb838a1
hanuz06 <lee1975uz@gmail.com>
2019-10-22 17:02:30 +0000

    amended typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,16 @@
-const sentence = "hello there from lighthouse labs\n";
+const sentence = "hello there from lighthouse labs";
 
-let delayT = 0;
-for (const char of sentence) {
-  delayT += 50;
-  setTimeout(() => {
-    // print the char here
-    process.stdout.write(char);
-  }, delayT); // <= 1s delay to make it noticeable. Can dial it down later.
-}
+let typewriter = str => {
+  let delayT = 0;
+  str = str.concat('\n');
+  
+  for (const char of str) {
+    delayT += 50;
+    setTimeout(() => {
+      // print the char here
+      process.stdout.write(char);
+    }, delayT); // <= 1s delay to make it noticeable. Can dial it down later.
+  }
+};
+
+typewriter(sentence);
